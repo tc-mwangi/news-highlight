1a90c4c97570602af65172f8e331bf598de19d45
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2019-02-02 15:48:49 +0300

    setup application factory

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+  app = Flask(__name__)
+
+  # creating app configurations
+  app.config.from_object(config_options[config_name])
+
+  # initializing flask extensions
+  bootstrap.init_app(app)
+
+  # add views and forms
+  return app
+
