1465f570a600dd2e875c8b8affb610375dd24f86
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2019-02-03 10:11:43 +0300

    display sources api call-successful

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect, url_for
 # from app import app
 from . import main
+from ..requests import get_source
 
 
 
@@ -10,5 +11,11 @@ def index():
 	'''
 	function returns index page and its data
 	'''
-	return render_template('index.html')
+	# Getting sources
+	news_sources = get_source('sources')
+	print(news_sources)
+	title = 'WithIt- Get current news.'
+	
+	
+	return render_template('index.html', title = title, sources = news_sources)
 	
--- app/requests.py
+++ app/requests.py
@@ -1,5 +1,7 @@
 import urllib.request, json
 from .models import Source
+from .models import Article 
+
 
 # getting api key
 api_key = None
@@ -11,6 +13,54 @@ def configure_request(app):
     global api_key, base_url
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
+
+def get_source(sources):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = base_url.format(sources,api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['sources']:
+            source_results_list = get_sources_response['sources']
+            source_results = process_results(source_results_list)
+
+
+    return source_results
+
+
+def process_results(source_list):
+    '''
+    Function  that processes the source result and transforms them to a list of Objects
+
+    Args:
+        source_list: A list of dictionaries that contain source details
+
+    Returns :
+        source_results: A list of source objects
+    '''
+    source_results = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        language = source_item.get('language')
+        country = source_item.get('country')
+
+        if name:
+            source_object = Source(id, name, description, url, category, language, country)
+            source_results.append(source_object)
+
+    return source_results
+
+
 
 
 
