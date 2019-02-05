f2db9f213a0974bceb41e706744828a0813cf8bc
tc-mwangi <wanjirumwangi.learn@gmail.com>
2019-02-05 10:37:33 +0300

    remove navbar

--- app/main/views.py
+++ app/main/views.py
@@ -17,15 +17,15 @@ def index():
 	print(news_sources)
 
 	# Getting top news
-	news_articles = get_article('articles')
-	print(news_articles)
+	# news_articles = get_article('articles')
+	# print(news_articles)
 	
 
 	
-	return render_template('index.html', title = title, sources = news_sources, articles = news_articles )
+	return render_template('index.html', title = title, sources = news_sources)
 
 # display articles by source
-@main.route('/article/<int:id>')
+@main.route('/article')
 def article():
 	'''
 	View articles page function that returns articles from a selected source
@@ -38,41 +38,3 @@ def article():
 	return render_template('article.html', articles = news_articles)
 
 
-# search for articles according to source name
-# @main.route('/search/<source_name>')
-# def search(source_name):
-#     '''
-#     View function to display the search results
-#     '''
-#     movie_name_list = movie_name.split(" ")
-#     movie_name_format = "+".join(movie_name_list)
-#     searched_movies = search_movie(movie_name_format)
-#     title = f'search results for {movie_name}'
-
-#     return render_template('search.html', movies = searched_movies)
-
-# get articles by source name view
-# @main.route('/movie/<movie_id>')
-# def movie(movie_id):
-#     '''
-#     view movie page that returns the details of a particular movie and its data.
-#     '''
-#     movie = get_movie(id)
-#     title = f'{ movie.title }'
-#     reviews = Review.get_reviews(movie.id)
-
-#     return render_template('movie.html', title = title, movie = movie, reviews = reviews)
-
-
-# search for articles according to source name
-# @main.route('/search/<movie_name>')
-# def search(movie_name):
-#     '''
-#     View function to display the search results
-#     '''
-#     movie_name_list = movie_name.split(" ")
-#     movie_name_format = "+".join(movie_name_list)
-#     searched_movies = search_movie(movie_name_format)
-#     title = f'search results for {movie_name}'
-
-#     return render_template('search.html', movies = searched_movies)
